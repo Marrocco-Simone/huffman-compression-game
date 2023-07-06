6e43147cdd4de5bde9c0afc2bac07f0d17a9dc14
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-07-07 00:20:14 +0200

    huffamn algorithm

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const letters = ["a", "b", "c", "d", "e"] as const;
 const max_lenght = 50;
 
+function getRandomNumber(max: number, min = 0) {
+  return Math.floor(Math.random() * max) + min;
+}
+
+function getRandomWorld() {
+  let s = "";
+  for (let i = 0; i < max_lenght; i++) {
+    s += letters[getRandomNumber(letters.length)];
+  }
+  return s;
+}
+
 export default function Home() {
-  function getRandomNumber(max: number, min = 0) {
-    return Math.floor(Math.random() * max) + min;
-  }
-
-  function getRandomWorld() {
-    let s = "";
-    for (let i = 0; i < max_lenght; i++) {
-      s += letters[getRandomNumber(letters.length)];
-    }
-    return s;
-  }
-
-  const [random_world, setRandomWorld] = useState(getRandomWorld());
+  const [random_world, setRandomWorld] = useState("");
+  // * otherwise server and client render different words and nextjs bothers
+  useEffect(() => setRandomWorld(getRandomWorld()), [setRandomWorld]);
 
   function getFrequencies() {
     const frequencies: {
@@ -31,6 +33,7 @@ export default function Home() {
     for (const letter of random_world.split("")) {
       if (frequencies[letter] == null)
         throw new Error("In random word there is a never seen letter");
+
       frequencies[letter]++;
     }
 
@@ -43,14 +46,54 @@ export default function Home() {
 
     for (const letter of letters) {
       elements.push(
-        <p key={letter}>
+        <p key={letter} className={styles.letter_description}>
           {letter}:{frequencies[letter]}
-          {"■".repeat(frequencies[letter])}
+          <input type="number" />
         </p>
       );
     }
 
     return elements;
+  }
+
+  function getHuffmanTree() {
+    type FrequencyTreeNode = {
+      letter?: string;
+      frequency: number;
+      left?: FrequencyTreeNode;
+      right?: FrequencyTreeNode;
+    };
+    const sortByFrequency = (a: FrequencyTreeNode, b: FrequencyTreeNode) => {
+      return a.frequency - b.frequency;
+    };
+
+    const frequencies = getFrequencies();
+    const queue: FrequencyTreeNode[] = [];
+    for (const letter of letters) {
+      if (frequencies[letter] == null)
+        throw new Error("In random word there is a never seen letter");
+
+      queue.push({
+        letter,
+        frequency: frequencies[letter],
+      });
+    }
+    queue.sort(sortByFrequency);
+
+    while (queue.length > 1) {
+      const z1 = queue.shift();
+      const z2 = queue.shift();
+      if (!z1 || !z2) break;
+
+      queue.push({
+        frequency: z1.frequency + z2.frequency,
+        left: z1,
+        right: z2,
+      });
+    }
+
+    console.log(queue);
+    return queue.shift();
   }
 
   return (
@@ -63,6 +106,11 @@ export default function Home() {
         </button>
       </div>
       <div className={styles.frequencies}>{getFrequenciesElement()}</div>
+      <div className={styles.random_world}>
+        Put here your encripted word:
+        <input typeof="number" />
+        <button onClick={() => console.log(getHuffmanTree())}>Check</button>
+      </div>
     </main>
   );
 }
