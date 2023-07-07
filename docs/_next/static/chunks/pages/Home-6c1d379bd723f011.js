06390d9b17740bf2ae174cb6dc12e74e377389e5
Marrocco-Simone <marrocco.simone00@gmail.com>
2023-07-07 23:28:31 +0200

    static files

---
+++ docs/_next/static/chunks/pages/Home-6c1d379bd723f011.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[336],{6834:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Home",function(){return u(8934)}])}},function(n){n.O(0,[934,774,888,179],function(){return n(n.s=6834)}),_N_E=n.O()}]);
